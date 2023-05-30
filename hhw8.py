db39aeecf5bd9db8125860cd2e06a592cbfa12a5
qquinyri1 <quinyrii@gmail.com>
2023-05-31 01:33:22 +0300

    Update hhw8.py

--- hhw8.py
+++ hhw8.py
@@ -1,42 +1,41 @@
-from datetime import datetime, timedelta
+import datetime
 
 def get_birthdays_per_week(users):
-    today = datetime.now().date()
-    start_of_week = today - timedelta(days=today.weekday())
-    end_of_week = start_of_week + timedelta(days=7)
+    today = datetime.date.today()
+    current_weekday = today.weekday() 
 
-    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+   
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-    birthdays_this_week = {}
+   
+    weekdays = weekdays[current_weekday:] + weekdays[:current_weekday]
+
+   
+    birthdays_per_week = {weekday: [] for weekday in weekdays}
+
+    
     for user in users:
-        name = user['name']
-        birthday = user['birthday'].date()
+        birthday = user['birthday']
+        if birthday.weekday() in [5, 6]: 
+           
+            monday = today + datetime.timedelta(days=(7 - current_weekday))
+            birthdays_per_week[weekdays[0]].append(user['name'])
+        else:
+           
+            birthday_weekday = weekdays[birthday.weekday()]
+            birthdays_per_week[birthday_weekday].append(user['name'])
 
-        if birthday >= start_of_week and birthday < end_of_week:
-            if birthday.weekday() >= 5:  
-                weekday = weekdays[0]  
-            else:
-                weekday = weekdays[birthday.weekday()]
-
-            if weekday not in birthdays_this_week:
-                birthdays_this_week[weekday] = []
-            birthdays_this_week[weekday].append(name)
-
-    if birthdays_this_week:
-        print("Список коллег для поздравления на этой неделе:")
-        for weekday, names in birthdays_this_week.items():
-            names_str = ", ".join(names)
-            print(f"{weekday}: {names_str}")
-    else:
-        print("На этой неделе нет дней рождения.")
+   
+    for weekday, names in birthdays_per_week.items():
+        if names:
+            print(f"{weekday}: {', '.join(names)}")
 
 
 users = [
-    {'name': 'Александр', 'birthday': datetime(2023, 5, 28)},
-    {'name': 'Елена', 'birthday': datetime(2023, 5, 30)},
-    {'name': 'Иван', 'birthday': datetime(2023, 6, 2)},
-    {'name': 'Мария', 'birthday': datetime(2023, 5, 26)},
-    {'name': 'Петр', 'birthday': datetime(2023, 6, 4)},
+    {'name': 'Bill', 'birthday': datetime.date(2000, 5, 30)},
+    {'name': 'Jill', 'birthday': datetime.date(2023, 5, 31)},
+    {'name': 'Kim', 'birthday': datetime.date(2024, 6, 2)},
+    {'name': 'Jan', 'birthday': datetime.date(2023, 6, 2)},
 ]
 
 get_birthdays_per_week(users)
